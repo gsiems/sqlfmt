a892bc881e05a57f8b042be88293a9d5a6cd6990
Gregory Siems <gsiems@gmail.com>
2020-10-17 01:03:20 -0500

    Add tagging of work units that belong to DDL statements

---
+++ ddl.go
@@ -0,0 +1,21 @@
+package main
+
+type ddl struct {
+	state int
+}
+
+/* tag iterates through the queue and tags the tokens that are believed
+to belong to DDL statements.
+
+ASSERTION: tagging DDL is the final tagging operation, therefore
+anything not otherwise tagged is considered to be DDL
+
+*/
+func (o *ddl) tag(q *queue) (err error) {
+	for i := 0; i < len(q.items); i++ {
+		if q.items[i].Type == Unknown {
+				q.items[i].Type = DDL
+		}
+	}
+	return err
+}
