78d7da6fc9b5fdc9b59bc1ee59006c4f7ef0aa29
Gregory Siems <gsiems@gmail.com>
2025-01-18 13:44:15 -0600

    Add additional test cases

--- testdata/input/postgresql/pg_wrapping.sql
+++ testdata/input/postgresql/pg_wrapping.sql
@@ -51,6 +51,106 @@ expression_11 THEN 19 WHEN expression_12 THEN 20 WHEN expression_13 THEN 21
 ELSE 22 END AS col_05;
 
 
+SELECT n.id,
+        n.name,
+        CASE WHEN n.is_active IS NULL THEN 'N' ELSE coalesce ( n.is_active, o.is_active ) END AS is_active,
+        CASE WHEN n.is_active IS NULL THEN 'N' ELSE coalesce ( n.is_active, o.is_active ) END AS is_also_active
+    FROM n ;
+
+with n as (
+select concat_ws ( ',',
+ CASE WHEN n.has_attrib_01       THEN 'attrib_01' END,
+ CASE WHEN n.has_attrib_02       THEN 'attrib_02' END,
+ CASE WHEN n.has_attrib_03       THEN 'attrib_03' END,
+ CASE WHEN n.has_attrib_04       THEN 'attrib_04' END,
+ CASE WHEN n.has_attrib_05       THEN 'attrib_05' END,
+ CASE WHEN n.has_attrib_06       THEN 'attrib_06' END,
+ CASE WHEN n.has_attrib_07       THEN 'attrib_07' END ) AS attributs,
+
+
+)
+
+select
+    n.precision + CASE
+        WHEN coalesce ( n.min_value, 0 ) < 0 THEN 1
+        ELSE 0
+        END + CASE WHEN coalesce ( n.scale, 0 ) > 0 THEN 1 ELSE 0 END AS max_char_length,
+
+    trim ( cast (
+            CASE
+        WHEN n.legacy_data_code
+                    = '000' THEN '42'
+        WHEN n.legacy_data_code
+                    = '001' THEN '43'
+        ELSE n.legacy_data_code
+        END AS text ) ) AS column_042,
+        CASE
+            WHEN func_02 ( n.something_something_01 ) IS NOT NULL THEN func_02 ( n.something_something_01 )
+            WHEN func_02 ( n.something_something_02 ) IS NOT NULL AND upper ( trim ( n.something_something_02 ) )
+                <> upper ( trim ( n.something_something_03 ) )
+                THEN trim ( n.something_something_04 )
+            END AS column_043,
+
+        coalesce ( n.parameter_01_cnt, 0 )
+            + coalesce ( n.parameter_02_cnt, 0 )
+            + coalesce ( n.parameter_03_cnt, 0 )
+            + coalesce ( n.parameter_04_cnt, 0 ) AS param_cnts,
+        CASE
+            WHEN coalesce ( n.parameter_01_cnt, 0 ) > 0
+                OR coalesce ( n.parameter_02_cnt, 0 ) > 0
+                OR coalesce ( n.parameter_03_cnt, 0 ) > 0
+                OR coalesce ( n.parameter_04_cnt, 0 ) > 0
+                THEN true
+            ELSE false
+            END AS has_params,
+
+
+        sum ( CASE WHEN n.name = '' AND parameter_03_cnt IS NOT NULL THEN parameter_03_cnt END ) AS cnt_03,
+        sum ( CASE WHEN n.name = 'qwer' AND parameter_04_cnt IS NOT NULL THEN parameter_04_cnt END ) AS cnt_04,
+        sum ( CASE WHEN n.name = 'qwertyui' AND parameter_05_cnt IS NOT NULL THEN parameter_05_cnt END ) AS cnt_05,
+        sum ( CASE WHEN n.name = 'qwertyuiopas' AND parameter_06_cnt IS NOT NULL THEN parameter_06_cnt END ) AS cnt_06,
+        sum ( CASE WHEN n.name = 'qwertyuiopasdfgh' AND parameter_07_cnt IS NOT NULL THEN parameter_07_cnt END ) AS cnt_07,
+        sum ( CASE WHEN n.name = 'qwertyuiopasdfghjklz' AND parameter_08_cnt IS NOT NULL THEN parameter_08_cnt END ) AS cnt_08,
+        CASE WHEN n.name = 'qwertyuiopasdfghjklzxcvb' AND n.some_parameter_09 THEN 'Yes' ELSE 'No' END  AS some_column_09,
+
+
+        CASE
+            WHEN n.order_date IS NOT NULL
+                THEN ( concat_ws ( ' ', to_char ( n.order_date, 'yyyy-mm-dd' ), coalesce ( to_char ( n.order_time, 'hh24:mi:ss' ), '00:00:00' ) ) )::timestamp without time zone
+            END AS column_09,
+        CASE
+            WHEN coalesce ( n.code, '' ) <> '' THEN n.resolve_code ( a_code => a_code )
+            WHEN coalesce ( a_some_param_name, '' ) <> '' THEN n.resolve_param ( a_param => a_some_param_name )
+            END AS column_10,
+        concat_ws ( ',',
+ CASE WHEN n.has_attrib_01       THEN 'attrib_01' END,
+ CASE WHEN n.has_attrib_02       THEN 'attrib_02' END,
+ CASE WHEN n.has_attrib_03       THEN 'attrib_03' END,
+ CASE WHEN n.has_attrib_04       THEN 'attrib_04' END,
+ CASE WHEN n.has_attrib_05       THEN 'attrib_05' END,
+ CASE WHEN n.has_attrib_06       THEN 'attrib_06' END,
+ CASE WHEN n.has_attrib_07       THEN 'attrib_07' END ) AS attributs,
+        case when n.name = '' then o.name::text || ' ' || o.col01::text else  p.name::text || ' ' || p.col01::text end as some_thing1,
+        case when n.name = 'qw' then o.name::text || ' ' || o.col02::text else  p.name::text || ' ' || p.col02::text end as some_thing2,
+        case when n.name = 'qwe' then o.name::text || ' ' || o.col03::text else  p.name::text || ' ' || p.col03::text end as some_thing3,
+        case when n.name = 'qwertyu' then o.name::text || ' ' || o.col04::text else  p.name::text || ' ' || p.col04::text end as some_thing4,
+2
+;
+
+SELECT column_01 IS NOT NULL
+            AND column_02 IS NOT NULL
+            AND column_01 > 10.0
+            AND column_01 < 20.0
+            AND column_02 > 100.0
+            AND column_02 < 200.0 ;
+
+SELECT column_01 IS NOT NULL
+            AND column_02 IS NOT NULL
+            AND ( ( column_01 > 10.0
+                    AND column_01 < 20.0 )
+                OR ( column_02 > 100.0
+                    AND column_02 < 200.0 ) ) ;
+
 SELECT coalesce (
             func_01 ( 'foo', 'bar', 42 ),
             func_02 ( 'foo', 'bar', 42 ),
@@ -120,3 +220,46 @@ select
                           ' other_id = ' || l_id_count ||
                           ' WHERE CURRENT OF ' || quote_ident(l_cursor::TEXT)
 ;
+
+
+CREATE OR REPLACE PROCEDURE src_schema.foo (
+    a_id in integer default null )
+LANGUAGE plpgsql
+AS $proc$
+
+BEGIN
+
+
+    call util_log.log_begin (
+        util_log.dici ( a_param_01 ),
+        util_log.dici ( a_param_02 ),
+        --util_log.dici ( a_param_03 ),
+        util_log.dici ( a_param_04 ),
+        util_log.dici ( a_param_05 ),
+        util_log.dici ( a_param_06 ),
+        util_log.dici ( a_param_07 ) ) ;
+
+    ------------------------------------------------------------------------
+    -- just barely long enough to wrap on commas
+    l_var_01 := concat_ws ( l_new_line, l_result, '', 'blah, blah, blah, blah, blah, b ' || a_parameter_name || ' ;' ) ;
+    l_var_02 := concat_ws ( l_new_line, l_result, '', 'blah, blah, blah, blah, blah, bl ' || a_parameter_name || ' ;' ) ;
+    l_var_03 := concat_ws ( l_new_line, l_result, '', 'blah, blah, blah, blah, blah, bla ' || a_parameter_name || ' ;' ) ;
+
+    ------------------------------------------------------------------------
+    -- more than long enough to wrap to wrap on both commas and concatenation operators
+    l_var_04 := concat_ws ( l_new_line, l_result, '', 'blah, blah, blah, blah, blah ' || a_parameter_name || ' ; -- some final comment text chucked in a literal to make things too long' ) ;
+
+    ------------------------------------------------------------------------
+    -- more than long enough to wrap on commas but not long enough to wrap on concatenation operators
+    l_var_05 := concat_ws (
+        l_new_line,
+        l_result,
+        '',
+        'blah, blah ' || a_parameter_name || ' blah, blah, blah ' || a_parameter_name || ' ;' ) ;
+
+    ------------------------------------------------------------------------
+    -- too short to wrap
+    l_var_06 := concat_ws ( l_new_line, l_result, 'blah, blah ' || a_parameter_name ) ;
+
+END ;
+$proc$ ;
