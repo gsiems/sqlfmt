8b8ac29ed614137875326aba03c5f7433078d368
Gregory Siems <gsiems@gmail.com>
2025-02-01 14:42:04 -0600

    Cleaned up (removed) old code

--- old_code/testdata/input/01-privs-pg-001.sql
+++
@@ -1,9 +0,0 @@
---d:postgres
-
-REVOKE ALL ON DATABASE tasker FROM PUBLIC ;
-GRANT CONNECT ON DATABASE tasker TO tasker_user ;
-
-
-GRANT ALL ON FUNCTION activity_is_parent_of ( integer, integer ) TO tasker_user ;
-
-REVOKE ALL ON FUNCTION activity_is_parent_of ( integer, integer ) FROM public ;
